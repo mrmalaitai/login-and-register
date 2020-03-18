bd74103ef10f3f2d4e585f15a1a78245cc7b57ca
mrmalaitai <62281287+mrmalaitai@users.noreply.github.com>
2020-03-19 07:51:12 +1300

    Added data in txt file and added Entry widgets to application.py

--- application.py
+++ application.py
@@ -4,6 +4,11 @@ import tkinter as tk
 
 root = tk.Tk()
 
+login_entry = tk.Entry(root)
+login_entry.grid(row = 0, column = 1)
+
+register_entry = tk.Entry(root)
+register_entry.grid(row = 1, column = 1)
 
 
 root.mainloop()
